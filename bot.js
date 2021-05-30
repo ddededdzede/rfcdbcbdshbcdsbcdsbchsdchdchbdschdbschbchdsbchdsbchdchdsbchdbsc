8d6eed0974234504c50ec8ccaf9675eb611d07a9
ddededdzede <85076505+ddededdzede@users.noreply.github.com>
2021-05-30 21:27:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,6 @@ bot.on("message", message => {
             .addField(`\`ping\``, `Check your bot's ping`)
             .addField(`\`kick\``, `Usage: **${config.prefix}kick [@User]**\n**${config.prefix}kick [@User][Reason]**`)
             .addField(`\`ban\``, `Usage: **${config.prefix}ban [@User]**\n**${config.prefix}ban [@User][Reason]**`)
-            .addField(`\`remove\``, `Removes a role from a user \nUsage: **${config.prefix}remove [@User] [Role]**`)
             .addField(`\`purge\``, `Clears a number of messages between 2 or 100 \nUsage: **${config.prefix}purge [number]**`)
             .addField(`\`rps\``, `Play rock paper scissors`)
             .addField(`\`say\``, `Have the bot say something`)
@@ -84,35 +83,6 @@ bot.on("message", message => {
                 message.channel.send(`${member.user.tag} was banned for ${reason}`);
             })
         }
-    }
-
-    if (command === "remove") {
-        if (!message.member.hasPermission('MANAGE_ROLES'))
-            return message.channel.send("Insufficient permissions (Requires permission `Manage roles`)").then(msg => {
-        msg.delete({ timeout: 30000 })
-    })
-        const member = message.mentions.members.first()
-        if (!member)
-            return message.channel.send("You have not mentioned a user").then(msg => {
-        msg.delete({ timeout: 30000 })
-    })
-        const remove = args.slice(1).join(" ")
-        if (!remove)
-            return message.channel.send("You have not specified a role").then(msg => {
-        msg.delete({ timeout: 30000 })
-    })
-        const roleRemove = message.guild.roles.cache.find(role => role.name === remove)
-        if (!roleRemove)
-            return message.channel.send("This role does not exist").then(msg => {
-        msg.delete({ timeout: 30000 })
-    })
-        if (!member.roles.cache.get(roleRemove.id))
-            return message.channel.send(`This user does not have the ${remove} role`).then(msg => {
-        msg.delete({ timeout: 30000 })
-    })
-        member.roles.remove(roleRemove.id).then((member) => {
-            message.channel.send(`${remove} removed from ${member.displayName}`)
-        })
     }
 
     if (command === "say") {
